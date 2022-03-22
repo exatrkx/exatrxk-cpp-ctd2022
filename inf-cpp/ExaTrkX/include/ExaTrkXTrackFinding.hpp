2e804d64f703d61d5b159ffbb7a3e9a03be61484
Xiangyang Ju <xiangyang.ju@gmail.com>
2022-03-22 12:17:25 -0700

    refactorize

---
+++ inf-cpp/ExaTrkX/include/ExaTrkXTrackFinding.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+#include <torch/torch.h>
+#include <torch/script.h>
+using namespace torch::indexing;
+
+class ExaTrkXTrackFinding
+{
+public:
+    struct Config{
+        std::string modelDir;
+
+        // hyperparameters in the pipeline.
+        int64_t spacepointFeatures = 3;
+        int embeddingDim = 8;
+        float rVal = 1.6;
+        int knnVal = 500;
+        float filterCut = 0.21;
+    };
+
+
+    ExaTrkXTrackFinding(const Config& config);
+    virtual ~ExaTrkXTrackFinding() {}
+
+    void getTracks(
+        std::vector<float>& inputValues,
+        std::vector<int>& spacepointIDs,
+        std::vector<std::vector<int> >& trackCandidates) const;
+
+    const Config& config() const { return m_cfg; }
+
+private:
+    void initTrainedModels();
+    torch::Tensor buildEdges(at::Tensor& embedFeatures, int64_t numSpacepoints);
+
+private:
+    Config m_cfg;
+    torch::jit::script::Module e_model;
+    torch::jit::script::Module f_model;
+    torch::jit::script::Module g_model;
+};
