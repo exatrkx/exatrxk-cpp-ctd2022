aeec5764658a5bdd908841ac0cbbafc66de2012f
xju <xju@cern.ch>
2022-05-12 14:53:36 -0700

    to compile

--- inf-cpp/make.sh
+++ inf-cpp/make.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cmake .. \
-    -DCMAKE_PREFIX_PATH=$(python -c 'import torch;print(torch.utils.cmake_prefix_path)') \
-    -DCMAKE_C_COMPILER=`which gcc` \
-    -DCMAKE_CXX_COMPILER=`which g++`
+	-DCMAKE_PREFIX_PATH="$(python -c 'import torch;print(torch.utils.cmake_prefix_path)');/workspace/build/third-party/protobuf/lib/cmake" \
+	-DCMAKE_C_COMPILER=`which gcc` \
+	-DCMAKE_CXX_COMPILER=`which g++`
